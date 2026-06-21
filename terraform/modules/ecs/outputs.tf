2e64ee7af37aaaab54da97d92d172df6a4fe4320
Aboubakr2000Cloud <aboubacre.ijannane@gmail.com>
2026-06-21 20:17:30 +0100

    Initial commit

---
+++ terraform/modules/ecs/outputs.tf
@@ -0,0 +1,5 @@
+output "cluster_name" { value = aws_ecs_cluster.this.name }
+output "cluster_arn" { value = aws_ecs_cluster.this.arn }
+output "service_name" { value = aws_ecs_service.this.name }
+output "task_definition" { value = aws_ecs_task_definition.this.arn }
+output "log_group_name" { value = aws_cloudwatch_log_group.this.name }
